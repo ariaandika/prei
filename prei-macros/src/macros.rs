21e6266ed3cece9dbd6f2d8491e53887199897e5
ariaandika <andikaakun17@gmail.com>
2025-02-24 18:55:19 +0700

    enum implementation

--- prei-macros/src/macros.rs
+++ prei-macros/src/macros.rs
@@ -4,16 +4,16 @@ use syn::{spanned::Spanned, *};
 use super::TsType;
 
 pub fn parse_derive(input: DeriveInput) -> Result<TokenStream> {
-    match &input.data {
-        syn::Data::Struct(data) => parse_struct(data.clone(), input),
-        syn::Data::Enum(_) => todo!(),
-        syn::Data::Union(_) => Err(Error::new(input.span(), "Union not supported"))
+    match input.data {
+        Data::Struct(_) => parse_struct(input),
+        Data::Enum(_) => parse_enum(input),
+        Data::Union(_) => Err(Error::new(input.span(), "Union not supported"))
     }
 }
 
-fn parse_struct(data: DataStruct, input: DeriveInput) -> Result<TokenStream> {
-    let ident = &input.ident;
-    let ident_str = input.ident.to_string();
+fn parse_struct(DeriveInput { ident, data, .. }: DeriveInput) -> Result<TokenStream> {
+    let Data::Struct(data) = data else { unreachable!("matches") };
+    let ident_str = ident.to_string();
     let head = format!("export type {ident_str} = ");
 
     let generated = match &data.fields {
@@ -80,6 +80,84 @@ fn parse_struct(data: DataStruct, input: DeriveInput) -> Result<TokenStream> {
 
             fn gen_type_to(buffer: &mut String) {
                 #generated;
+            }
+        }
+    })
+}
+
+fn parse_enum(DeriveInput { ident, data, .. }: DeriveInput) -> Result<TokenStream> {
+    let Data::Enum(data) = data else { unreachable!("matched") };
+    let ident_str = ident.to_string();
+    let head = format!("export type {ident_str} = ");
+
+    let generated = data.variants.into_iter().map(|variant| {
+        match &variant.fields {
+            Fields::Named(fields) => {
+                let fields = fields
+                    .named
+                    .iter()
+                    .map(|e| e.ident.as_ref().expect("named").to_string())
+                    .zip(&variant.fields)
+                    .map(|(id, e)| {
+                        let ty = &e.ty;
+                        let head = format!("    {id}: ");
+                        quote! {
+                            buffer.push_str(#head);
+                            <#ty as #TsType>::gen_id_to(buffer);
+                            buffer.push_str(",\n");
+                        }
+                    });
+
+                let head = format!("  | {{\n    tag: {:?},\n    value: {{",variant.ident);
+                quote! {
+                    buffer.push_str(#head);
+                    #(#fields)*
+                    buffer.push_str("    }\n  }\n");
+                }
+            },
+            Fields::Unnamed(fields) if fields.unnamed.len() == 1 => {
+                let ty = fields.unnamed.first().expect("one");
+                let head = format!("  | {{\n    tag: {:?},\n    value: ",variant.ident);
+                quote! {
+                    buffer.push_str(#head);
+                    <#ty as #TsType>::gen_id_to(buffer);
+                    buffer.push_str("\n  }\n");
+                }
+            },
+            Fields::Unnamed(fields) => {
+                let fields = fields.unnamed.iter().map(|e|{
+                    let ty = &e.ty;
+                    quote! {
+                        <#ty as #TsType>::gen_id_to(buffer);
+                        buffer.push_str(",");
+                    }
+                });
+                let head = format!("  | {{\n    tag: {:?},\n    value: [",variant.ident);
+                quote! {
+                    buffer.push_str(#head);
+                    #(#fields)*;
+                    buffer.push_str("]\n  }\n");
+                }
+            }
+            Fields::Unit => {
+                let head = format!("  | {{\n    tag: {:?},\n    value: null\n  }}\n",variant.ident);
+                quote! {
+                    buffer.push_str(#head);
+                }
+            },
+        }
+    });
+
+    Ok(quote! {
+        #[cfg(debug_assertions)]
+        impl #TsType for #ident {
+            fn gen_id_to(buffer: &mut String) {
+                buffer.push_str(#ident_str);
+            }
+
+            fn gen_type_to(buffer: &mut String) {
+                buffer.push_str(#head);
+                #(#generated)*
             }
         }
     })
